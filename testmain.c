59626ada081f7e45f2978342a4a602316eccbda1
Tatamize <news@interest-a.com>
2022-03-23 12:38:35 +0100

    add main and Makefile but still in construction

---
+++ testmain.c
@@ -0,0 +1,22 @@
+#include "minishell.h"
+
+int	 main()
+{
+	char		*prompt;
+	char		*line = NULL;
+	HIST_ENTRY	*history = NULL;
+
+	prompt = getenv("PS1");
+	printf("PS1 is %s\n", prompt);
+	if (prompt == NULL)
+		prompt = "minishell > ";
+	while ((line = readline(prompt)))
+	{
+		add_history(line);
+		free(line);
+	}
+	rl_clear_history();
+	printf(" last input = [ %s ]\n", line);
+	free(line);
+	return (0);
+}
