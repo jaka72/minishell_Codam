848e0a450e683a7c122db0230ffce76ed6a35569
Tatamize <news@interest-a.com>
2022-05-19 14:20:43 +0200

    small modification

--- builtins/export.c
+++ builtins/export.c
@@ -1,5 +1,20 @@
 #include "minishell.h"
 
+void	print_env_export(t_infos *info)
+{
+	t_env	*env;
+
+	env = info->start_env;
+	while (env->next)
+	{
+		printf("declare -x ");
+		printf("%s=\"%s\"\n", env->name, env->value);
+		env = env->next;
+	}
+	printf("declare -x ");
+	printf("%s=\"%s\"\n", env->name, env->value);
+}
+
 t_env	*find_and_split(const char *s, char c, t_env *env)
 {
 	int	i;
@@ -44,22 +59,29 @@ int	run_export_builtin(t_cmd *cmd, t_infos *info)
 	temp_env = malloc(sizeof(t_env) * 1);
 	if (temp_env == NULL)
 		errtext_exit("for temp_env malloc failed");
-	while(cmd->args[i])
+	if (cmd->args[1] == NULL)
 	{
-		printf("%s\n", cmd->args[i]);
-		if (cmd->args[1][0] == '=')
-		{
-			printf("%s : not a valid identifier\n", cmd->args[i]);
-			return (1);
-		}
-		if (ft_strchr(cmd->args[i], '=') != NULL)
-		{
-			temp_env = find_and_split(cmd->args[i], '=', temp_env);
-			find_name_delate(temp_env->name, info); // to find if the same name variable is exist 
-			end_env = last_env(info->start_env);
-			end_env->next = temp_env;
-		}
-		i++;
+		print_env_export(info);
 	}
+	else{
+		while(cmd->args[i])
+		{
+			printf("%s\n", cmd->args[i]);
+			if (cmd->args[1][0] == '=')
+			{
+				printf("%s : not a valid identifier\n", cmd->args[i]);
+				return (1);
+			}
+			if (ft_strchr(cmd->args[i], '=') != NULL)
+			{
+				temp_env = find_and_split(cmd->args[i], '=', temp_env);
+				find_name_delate(temp_env->name, info); // to find if the same name variable is exist 
+				end_env = last_env(info->start_env);
+				end_env->next = temp_env;
+			}
+			i++;
+		}
+	}
+
 	return (0);
 }
